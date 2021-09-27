995e682040a469e37f74c4ca529fe46cc2fdae29
Reza <99068159@mydavinci.nl>
2021-09-27 14:02:27 +0200

    next

---
+++ Cluedo.py
@@ -0,0 +1,14 @@
+print("""-------------------------------------------------------------
+|                       Solicitatie Cluedo                  |
+-------------------------------------------------------------""")
+
+naam = input("Hoe heet u? :")
+
+Leeftijd = int(input("Hoe oud bent u? :"))# 2e opdracht
+Geslacht = input("Bent u een man of een vrouw? M/V :")#1 Van de drie personage vragen
+Ervaring = int(input("Heeft u al acteer ervaring? zo ja, hoeveel jaar dan? :")) #Verplicht voor aduitie
+Spel = input("Heeft u ooit het spel gespeeld? J/N :") #a 2e opdracht
+KleurHaar = input("Wat voor kleur haar heeft u? :")#1 van de drie personage vragen
+Lengte = int(input("Hoe lang bent u in cm? :"))#1 van de drie personage vragen
+
+
