fd4f7f5639ae04d50b44e64d91b5812a87c477ef
Reza <99068159@mydavinci.nl>
2021-09-27 14:23:57 +0200

    next

--- Cluedo.py
+++ Cluedo.py
@@ -8,7 +8,13 @@ Leeftijd = int(input("Hoe oud bent u? :"))# 2e opdracht
 Geslacht = input("Bent u een man of een vrouw? M/V :")#1 Van de drie personage vragen
 Ervaring = int(input("Heeft u al acteer ervaring? zo ja, hoeveel jaar dan? :")) #Verplicht voor aduitie
 Spel = input("Heeft u ooit het spel gespeeld? J/N :") #a 2e opdracht
-KleurHaar = input("Wat voor kleur haar heeft u? :")#1 van de drie personage vragen
+KleurHaar = input("Wat voor kleur haar heeft u? Antwoord in één volle kleur:")#1 van de drie personage vragen
 Lengte = int(input("Hoe lang bent u in cm? :"))#1 van de drie personage vragen
 
-
+if Ervaring > 3 and Leeftijd > 24 and Spel == "J": #Ik vindt dat dit meer dan 3 jaar moet zijn
+    if Leeftijd >30 and Geslacht == ("M") and KleurHaar == "Grijs" or "Wit" and Lengte >= 180: #Kolonel van Geelen
+        print("Je mag op auditie voor de rol van Kolonel van Geelen")
+    elif Leeftijd >40 and Geslacht == ("V") and KleurHaar == "Grijs" or "Wit" and Lengte >120 <150: #Dominee groenewoud
+        print("Je mag op auditie voor de rol van Dominee Groenewoud") 
+    else:
+        print("Je mag helaas niet op auditie")
